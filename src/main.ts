60904ec9e5d5423b78c0f4e3f32a5ca0961e3f26
abazunts <bazunc@gmail.com>
2022-11-09 16:57:11 +0400

    ascsd

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/v1', app, document);
 
   // Listen app
   await app.listen(PORT)
